7f93af21d88899cefb900aa727f1a6e75270f565
Tim Hagemann <tim@infinityblade.de>
2016-04-17 12:51:55 +0200

    Some comments for the java implementation.

--- src/main/java/de/eternalwings/uni/Slow.java
+++ src/main/java/de/eternalwings/uni/Slow.java
@@ -9,15 +9,25 @@ public class Slow {
     private static final String FILENAME = "./src/main/resources/i2t.csv";
 
     public static void main(String[] args) throws IOException {
-        Stack<IPRange> ranges = new Stack<>();
+        // Reader for file
         BufferedReader reader = new BufferedReader(new FileReader(new File(FILENAME)));
+
+        // Stack for data
+        Stack<IPRange> ranges = new Stack<>();
         String line;
+
+        // Go through all the lines of the file
         while((line = reader.readLine()) != null) {
+
+            // "Parse" them into a nicer format
             String[] split = line.split(",");
             IPRange range = IPRange.apply(BigInt.apply(split[0]), BigInt.apply(split[1]), split[2]);
+
+            // Push them on the stack
             ranges.push(range);
         }
 
+        // Do something with the data
         System.out.println(ranges.size());
     }
 }
