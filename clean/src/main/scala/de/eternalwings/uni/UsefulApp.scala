820304f80be0637c2e4367bcdfe42d0dd04cf890
Tim Hagemann <tim@infinityblade.de>
2016-04-22 10:24:35 +0200

    Finishing touches

--- clean/src/main/scala/de/eternalwings/uni/UsefulApp.scala
+++ clean/src/main/scala/de/eternalwings/uni/UsefulApp.scala
@@ -1,8 +1,12 @@
 package de.eternalwings.uni
 
-import akka.actor.{ActorSystem, Props}
+import akka.actor.{ActorContext, ActorSystem, Props}
 
 object UsefulApp extends App {
     val system = ActorSystem("UsefulReaderApp")
     val fileLoc = "./src/main/resources/i2t.csv"
+
+    def getActor(actorContext: ActorContext, name: String) = {
+        context.actorSelection("/user/" + name)
+    }
 }
