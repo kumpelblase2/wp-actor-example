72de1fa4a25280a95b054d5fd7a34f13c1fcec8b
Tim Hagemann <tim@infinityblade.de>
2016-05-26 16:07:17 +0200

    Use threads in java example.

--- src/main/java/de/eternalwings/uni/Slow.java
+++ src/main/java/de/eternalwings/uni/Slow.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.*;
 
 public class Slow {
-    private static class IPRange {
+    private static class IPRange { // Structure for holding the IP data
         final String start;
         final String end;
         final String timezone;
@@ -23,71 +23,87 @@ public class Slow {
 
     private static final String FILENAME = "./src/main/resources/i2t.csv";
 
-    public static void main(String[] args) throws IOException {
-        // Reader for file
-        BufferedReader reader = new BufferedReader(new FileReader(new File(FILENAME)));
-        String line;
+    private final Queue<String> lines = new LinkedList<>(); // Queue for lines that were read and need to be processed
+    private final Object linesLock = new Object(); // Mutex for access to the lines
+    private final Queue<IPRange> ranges = new LinkedList<>(); // Queue
+    private final Object rangesLock = new Object();
+    private String filename;
+    private boolean done = false;
+    private boolean done2 = false;
 
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Slow s = new Slow();
+        s.filename = FILENAME;
+        Thread reader = new Thread(s::readLines); // Thread for reading lines
+        reader.start();
+        Thread parser = new Thread(s::parseLines); // Thread for parsing lines
+        parser.start();
+        Thread calculator = new Thread(s::calculate); // Thread for calculating something with parsed lines
+        calculator.start();
+        calculator.join(); // Wait for the last thread to finish
+    }
+
+    private void readLines() {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(new File(filename)));
+            String line;
+            // Go through all the lines of the file
+            while((line = reader.readLine()) != null) {
+                synchronized (linesLock) {
+                    lines.offer(line);
+                }
+            }
+        } catch(IOException ignored) {
+            System.out.println("Cannot read file");
+        }
+
+        done = true;
+    }
+
+    private void parseLines() {
+        while(!done || !lines.isEmpty()) {
+            String line;
+            synchronized (linesLock) {
+                line = lines.poll();
+                if(line == null) {
+                    continue;
+                }
+            }
+
+            String[] split = line.split(",");
+            IPRange range = new IPRange(split[2], intToIP(Long.valueOf(split[1])), intToIP(Long.valueOf(split[0])));
+            synchronized (rangesLock) {
+                if(!ranges.offer(range)) {
+                    System.out.println("Cannot offer!");
+                }
+            }
+        }
+
+        done2 = true;
+    }
+
+    private void calculate() {
         // Some data that we want to get out of all data
         Map<String, Integer> occurrences = new HashMap<>();
 
-        // Go through all the lines of the file
-        while((line = reader.readLine()) != null) {
+        while(!done2 || !ranges.isEmpty()) {
+            IPRange range;
+            synchronized (rangesLock) {
+                range = ranges.poll();
+                if(range == null) {
+                    continue;
+                }
 
-            // "Parse" them into a nicer format
-            String[] split = line.split(",");
-            IPRange range = new IPRange(split[2], intToIP(Long.valueOf(split[1])), intToIP(Long.valueOf(split[0])));
-
-            // Do some work with it
-            String firstPart = range.getFirst();
-            occurrences.put(firstPart, occurrences.getOrDefault(firstPart, 0) + 1);
+                String firstPart = range.getFirst();
+                occurrences.put(firstPart, occurrences.getOrDefault(firstPart, 0) + 1);
+            }
         }
 
         // Do something with the data
         System.out.println(occurrences.toString());
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    static String intToIP(long ip) {
+    private static String intToIP(long ip) {
         List<String> parts = new ArrayList<>();
         for(int i = 0; i < 4; i++) {
             parts.add("" + ((ip >> i * 8) % 256));
